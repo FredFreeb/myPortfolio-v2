d447000d841d4c521dee7995b6c3b5ba2a7585df
FredFribel <frederic.fribel@hotmail.fr>
2023-04-14 01:02:32 +0200

    first commit

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myButton");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+}
+}
+////////////////// effet multivers
+let myDiv = document.getElementById('showFullscreenButton');
+		let toggleFullscreenButton = document.getElementById('toggleFullscreenButton');
+
+		toggleFullscreenButton.addEventListener('click', function() {
+			if (!document.fullscreenElement) {
+				myDiv.requestFullscreen();
+			} else {
+				document.exitFullscreen();
+			}
+		});
